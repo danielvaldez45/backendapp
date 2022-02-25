0845bce6cbccabd963dd81adc9e7fadd1600525d
daniel torres <danieltorres1552@gmail.com>
2022-02-25 12:27:53 -0600

    Se creo una respuesta personalizada para el cliente, en la ruta: /login

--- src/main/java/com/phoenix/backend/auth/AuthController.java
+++ src/main/java/com/phoenix/backend/auth/AuthController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class AuthController {
@@ -23,21 +29,27 @@ public class AuthController {
         return true;
     }
 
-    @PostMapping("/login")
-    boolean Login(@RequestBody UserAuth userAuth) {
+    @PostMapping(value = "/login")
+    ResponseEntity<Object> Login(@RequestBody UserAuth userAuth) {
         //Recuperamos los parametros de la peticion.
         String username = userAuth.getUsername();
         String password = userAuth.getPassword();
-        
+
+        //Ejecutamos el stored procedured
         boolean isExistsUser;
         isExistsUser = repository.sp_get_auths_verify_user_estadiadvt(username, password);
-        
-        ResponseHandler res = new ResponseHandler();
-        
-        if (isExistsUser) {
-            return true;
-            //return ResponseHandler.generateResponse("Successfully added data!", HttpStatus.OK, true);
+
+        ResponseHandler response = new ResponseHandler();
+        try {
+            if (isExistsUser) {
+                //Aqui necesito response con un json.
+                return response.generateResponse("Hola mundo", HttpStatus.OK, userAuth);
+                //return response;
+                //return ResponseHandler.generateResponse("Successfully added data!", HttpStatus.OK, true);
+            }
+        } catch (HttpMessageNotWritableException ex) {
+            ex.printStackTrace();
         }
-        return false;
+        return response.generateResponse("hola mundo", HttpStatus.I_AM_A_TEAPOT, userAuth);
     }
 }
